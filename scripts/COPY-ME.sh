90a8edfaefd3041005cad6f6f7f776819cb721c2
tjmaynes <tj@tjmaynes.com>
2022-08-14 16:56:15 -0500

    chore(tailscale): ensure tailscale tunnel exists

--- scripts/COPY-ME.sh
+++ scripts/COPY-ME.sh
@@ -5,15 +5,32 @@ set -e
 export BASE_DIRECTORY=$1
 export PLEX_CLAIM_TOKEN=$2
 
-function main() {
-  if [[ -d "config" ]]; then
-	  sudo rm -rf config
+function ensure_tailscale_tunnel_exists() {
+  if [[ ! -c "/dev/net/tun" ]]; then
+    if [[ ! -d "/dev/net" ]]; then
+      mkdir -m 755 /dev/net
+    fi
+
+    mknod /dev/net/tun c 10 200
+    chmod 0755 /dev/net/tun
   fi
 
-  curl -SL https://github.com/tjmaynes/config/archive/master.tar.gz | tar xz
-  mv config-main config
+  if ( !(lsmod | grep -q "^tun\s") ); then
+    insmod /lib/modules/tun.ko
+  fi
+}
 
-  pushd config/hosts/kratos
+function main() {
+  if [[ -d "kratos" ]]; then
+	  sudo rm -rf kratos
+  fi
+
+  curl -SL https://github.com/tjmaynes/kratos/archive/master.tar.gz | tar xz
+  mv kratos-main kratos
+
+  ensure_tailscale_tunnel_exists
+
+  pushd kratos
     ./scripts/install.sh "$BASE_DIRECTORY" "$PLEX_CLAIM_TOKEN"
   popd
 }
